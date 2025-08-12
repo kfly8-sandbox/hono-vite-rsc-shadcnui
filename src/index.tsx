b58709db2828e2c81dc4ab08af10e1c8aa16d158
kobaken <kentafly88@gmail.com>
2025-08-12 22:16:37 +0900

    Use shadcn/ui but not working

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
 
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+
 const app = new Hono()
 
 app.use(renderer)
 
+function Root() {
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">Hello!</h1>
+     <Button className="mt-4" onClick={() => console.log('hello')}>Click Me</Button>
+     <Checkbox />
+    </>
+  )
+}
+
 app.get('/', (c) => {
-  return c.render(<h1 className="text-3xl font-bold underline">Hello!</h1>)
+  return c.render(<Root />)
 })
 
 export default app
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite'
 import ssrPlugin from 'vite-ssr-components/plugin'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 export default defineConfig({
   ssr: {
     external: ['react', 'react-dom']
   },
-  plugins: [cloudflare(), ssrPlugin(), tailwindcss()]
+  plugins: [cloudflare(), ssrPlugin(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
