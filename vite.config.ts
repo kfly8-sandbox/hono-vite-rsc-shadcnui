9ed26671f84bf00e5ce6749078668a35d2b6c05b
kobaken <kentafly88@gmail.com>
2025-08-12 22:04:14 +0900

    Use tailwindcss@v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite'
 import ssrPlugin from 'vite-ssr-components/plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   ssr: {
     external: ['react', 'react-dom']
   },
-  plugins: [cloudflare(), ssrPlugin()]
+  plugins: [cloudflare(), ssrPlugin(), tailwindcss()]
 })
