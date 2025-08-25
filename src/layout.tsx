a264a91ae23e6c99940e5ab89ab1c75f9d64ac4a
kobaken <kentafly88@gmail.com>
2025-08-25 13:56:15 +0900

    Update

--- src/layout.tsx
+++ src/layout.tsx
@@ -16,7 +16,7 @@ export const Layout: React.FC<Props> = ({ children, title }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <title>{title ?? "hono-vite-rsc-shadcnui" }</title>
         { !import.meta.env.PROD && <ViteClient /> }
-        <Link href="/src/style.css" rel="stylesheet" />
+        { !import.meta.env.PROD && <Link href="/src/style.css" rel="stylesheet" /> }
       </head>
       <body>{children}</body>
     </html>
